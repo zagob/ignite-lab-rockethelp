9e7c9a559ee0badb2d8470b32bd86fd71ab82d34
zagob <matheusus1_23@hotmail.com>
2022-07-21 18:23:10 -0300

    implement firebase + config

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,45 @@
 import { Heading, VStack, Text, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 import { useState } from "react";
+import auth from "@react-native-firebase/auth";
 
 import Logo from "../assets/logo_primary.svg";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { Alert } from "react-native";
 
 export function SignIn() {
-  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log("sd");
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        console.log(err.code);
+        setIsLoading(false);
+
+        if(err.code === 'auth/invalid-email') {
+          return Alert.alert('Entrar', 'E-mail inválido.')
+        }
+
+        if(err.code === 'auth/user-not-found') {
+          return Alert.alert('Entrar', 'Usuário não cadastrado.')
+        }
+
+        return 
+      });
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -28,7 +57,7 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
@@ -38,7 +67,12 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" onPress={() => {}} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 }
